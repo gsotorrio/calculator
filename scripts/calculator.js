980a9e1f858c87de827149ab4b36eb14a57bbb8c
gsotorrio <willybosko@gmail.com>
2016-09-28 18:14:31 +0200

    changing backgroundColor

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,6 +1,10 @@
-
+"use strict";
 
 function displayNumber(itemClicked){
 	var result = window.document.getElementById("result");
 	result.innerText += itemClicked.innerText;	
+}
+
+function changeColor(itemClicked){
+	itemClicked.style.backgroundColor= "#f34";
 }
